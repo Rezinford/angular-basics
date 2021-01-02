a7bfb3ea40605954df57d2d6e6cebb30f7284afa
zendering <rezinford777@gmail.com>
2021-01-02 17:52:37 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // title = 'Dynamic title';
+  // number = 42;
+  // arr = [1, 2, 3];
+  // obj = {a: 1, b: {c: 3}};
+  // img = 'https://avatarko.ru/img/kartinka/27/film_anonim_26395.jpg';
+  // img2 = 'https://avatarko.ru/img/kartinka/1/avatarko_anonim.jpg';
+  // inputValue = '';
+  //
+  // constructor() {
+  //   // setTimeout(() => {
+  //   //   this.img = this.img2;
+  //   //   }, 5000);
+  // }
+  //
+  // onInput(event?: KeyboardEvent) {
+  //   console.log('Event', event);
+  //   // this.inputValue = event.target.value;
+  //   this.inputValue = (event.target as HTMLInputElement).value;
+  // }
+  //
+  // onBlur(str: string) {
+  //   this.inputValue = str;
+  // }
+  //
+  // onClick() {
+  //   console.log('Click');
+  // }
+  // title = ' Initial';
+  //
+  // onInput(event: any) {
+  //   this.title = event.target.value;
+  // }
+
+  toggle = false;
+}
+
+

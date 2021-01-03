270977ba9df95a5a379cc4d96fc9bc95d4efa928
zendering <rezinford777@gmail.com>
2021-01-03 09:23:35 -0500

    2_19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,23 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13];
+  obj = [{
+    title: 'Post 1', author: 'Vladlen',
+    comments: [
+      {name: 'Max', text: 'Lorem 1 '},
+      {name: 'Max', text: 'Lorem 2 '},
+      {name: 'Max', text: 'Lorem 3 '}
+    ]},
+    {
+      title: 'Post 2', author: 'Vladlen 2',
+      comments: [
+        {name: 'Max 2', text: 'Lorem 1 '},
+        {name: 'Max 2', text: 'Lorem 2 '},
+        {name: 'Max 2', text: 'Lorem 3 '}
+      ]}
+  ];
+
 }
 
 

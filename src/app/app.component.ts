29b58bc8c5b5cb908e508e1140c48a185bbfe9c9
zendering <rezinford777@gmail.com>
2021-01-03 17:06:25 -0500

    3_27

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,19 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- posts: Post[] = [
-   { title: 'sdfsdf',  text: 'sdfsf', id: 1},
-   {title: 'sdfs', text: 'sdffsf', id: 12}
-   ];
+  posts: Post[] = [
+    {title: 'sdfsdf', text: 'sdfsf', id: 1},
+    {title: 'sdfs', text: 'sdffsf', id: 12}
+  ];
 
   updatePosts(post: Post) {
     this.posts.unshift(post);
     // console.log('Post', post);
+  }
+
+  removePosts(id: number) {
+    console.log('Id to remove', id);
+    this.posts = this.posts.filter(p => p.id !== id);
   }
 }
 

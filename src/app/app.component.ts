f87279c1208d2daa8e62a4515087bac514d2e316
zendering <rezinford777@gmail.com>
2021-01-03 12:55:33 -0500

    3_22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+ posts: Post[] = [
+   { title: 'sdfsdf',  text: 'sdfsf', id: 1},
+   {title: 'sdfs', text: 'sdffsf', id: 12}
+   ];
 }
 
 

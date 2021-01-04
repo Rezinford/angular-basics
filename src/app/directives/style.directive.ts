163401ee4b2604b461095f06021e1aee88479a90
zendering <rezinford777@gmail.com>
2021-01-04 17:41:34 -0500

    3_32

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 import {ElementAnimationState} from '@angular/animations/browser/src/render/transition_animation_engine';
 
 @Directive({
@@ -6,9 +6,18 @@ import {ElementAnimationState} from '@angular/animations/browser/src/render/tran
 })
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    // this.r.setStyle(this.el.nativeElement, 'color', 'blue');
     console.log(el);
     // el.nativeElement.style.color = 'red';
   }
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event);
+  }
+  @HostListener('mouseenter') onEnter() {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+  }
 
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null);
+  }
 }

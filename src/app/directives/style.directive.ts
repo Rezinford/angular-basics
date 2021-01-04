02a5d55629b53398d8c2e277e2becae0c30aff79
zendering <rezinford777@gmail.com>
2021-01-04 04:42:26 -0500

    3_31

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,14 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {ElementAnimationState} from '@angular/animations/browser/src/render/transition_animation_engine';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  constructor(private el: ElementRef, private r: Renderer2) {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    console.log(el);
+    // el.nativeElement.style.color = 'red';
+  }
 
 }

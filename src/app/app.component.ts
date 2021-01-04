ce4088b073f92a30e0276081996a356b82586949
zendering <rezinford777@gmail.com>
2021-01-04 03:36:44 -0500

    3_30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  posts: Post[] = [
-    {title: 'sdfsdf', text: 'sdfsf', id: 1},
-    {title: 'sdfs', text: 'sdffsf', id: 12}
-  ];
+export class AppComponent {
 
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-    // console.log('Post', post);
-  }
-
-  removePosts(id: number) {
-    console.log('Id to remove', id);
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
-
-  ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log('timeout');
-    //   this.posts[0] = {
-    //     title: 'chenchd',
-    //     text: 'chenched 2',
-    //     id: 33
-    //   };
-    // }, 5000);
-  }
 }
 
 

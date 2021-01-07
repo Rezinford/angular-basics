e955b9bfe80993425814b2570adad7e594d00efe
zendering <rezinford777@gmail.com>
2021-01-07 09:08:51 -0500

    3_36

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {StyleDirective} from './directives/style.directive';
-import { IfontDirective } from './directives/ifont.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StyleDirective,
-    IfontDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,

61fc1cacef39a5f9407a4550e9f1e9c500333c8a
zendering <rezinford777@gmail.com>
2021-01-07 15:15:24 -0500

    3_40

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,18 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent {
   e: number = Math.E;
   str = 'hello word';
+  date: Date = new Date();
+  float: number = 0.42;
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  };
 }
 
 

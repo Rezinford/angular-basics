b0adc4fa41a3b7cb1eecd3ec0f48d12bade3d044
zendering <rezinford777@gmail.com>
2021-01-08 18:19:02 -0500

    3_48

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {AppCounterServesis} from './servesis/app-counter.servesis';
+import {LocalCounterService} from './servesis/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService]
 })
 export class AppComponent {
 
-  constructor(private appCounterService: AppCounterServesis) {
+  constructor(private appCounterService: AppCounterServesis,
+              private localCounterService: LocalCounterService) {
 
   }
 }

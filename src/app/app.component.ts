ef0dd70eaf40b51b557cccddc05af3b88521f274
zendering <rezinford777@gmail.com>
2021-01-08 15:24:47 -0500

    3_43

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 
+
+export interface Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Bear', text: 'Somsin'},
+    {title: 'Bread', text: 'Somsin 2'},
+    {title: 'JavaScript', text: 'EEEEEE'}
+  ];
+  search: '';
+  searchField = 'title';
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {MultiByPipe} from './pipes/multi-by.pipe';
 import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MultiByPipe,
-    ExMarksPipe
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,

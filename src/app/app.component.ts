74a8f56d882e26814acf6828e2fd89795be5c80e
zendering <rezinford777@gmail.com>
2021-01-08 18:00:05 -0500

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-
-
-export interface Post {
-  title: string;
-  text: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise Resolve');
-    }, 4000);
-  });
-
-  date$: Observable<Date> = new Observable(obs => {
-    setTimeout(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
-
-  date: Date;
-
-  ngOnInit(): void {
-    this.date$.subscribe(
-      date => {
-        this.date = date;
-      }
-    );
-  }
+export class AppComponent {
 }
 
 
